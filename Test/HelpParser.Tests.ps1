51cd12a56491299658049c4cd044264a493fd70f
Jon Carrier <jjcarrier@gmail.com>
2023-07-20 22:41:14 -0400

    Improved test logic
    Added more thorough validation of parsed results for a subset of
    tests.
    
    Added test logic for param value parsing.

--- Test/HelpParser.Tests.ps1
+++ Test/HelpParser.Tests.ps1
@@ -2,55 +2,158 @@
 
 BeforeAll {
   Import-Module "$PSScriptRoot\..\HelpParser\HelpParser.psm1"
+
+  class MockCommandAst {
+    [array]$CommandElements
+
+    MockCommandAst([string]$command) {
+      $this.CommandElements = $command.Split()
+    }
+
+    [string]ToString() {
+      return $this.CommandElements -join ' '
+    }
+  }
 }
 
-# TODO: Fix parsing these lines
-# -h,-H,--help,-help,-usage,/? = Print usage information and exit.
-# --version,-version,/V [<file>]
-#
-
 Describe 'Get-ParsedHelpFlag' {
-  It "The supplied cmake help output contains 25 flags" {
+  It "parses 28 flags from the supplied cmake help output" {
     $flags = Get-ParsedHelpFlag -HelpData (Get-Content "$PSScriptRoot\TextTests\cmake.txt")
     # NOTE: Currently this implementation is only expected to expand "-L[A][H]" to two variants
     # (does not cover all permutations).
-    $flags.Count | Should -Be 28
+    $expectedFlags = @(
+      '-A',
+      '-B',
+      '-C',
+      '-D',
+      '-E',
+      '-G',
+      '-h',
+      '-H',
+      '-help',
+      '-L',
+      '-LAH',
+      '-N',
+      '-P',
+      '-S',
+      '-T',
+      '-U',
+      '-usage',
+      '-version',
+      '-Wdeprecated',
+      '-Wdev',
+      '-Werror=',
+      '-Werror=',
+      '-Wno-deprecated',
+      '-Wno-dev',
+      '-Wno-error=',
+      '-Wno-error=',
+      '/?',
+      '/V'
+    )
+
+    $flags.Count | Should -Be $expectedFlags.Count
+    ($flags.Param -join ' ') -eq ($expectedFlags -join ' ') | Should -Be $true
   }
 
-  It "The supplied make help output contains 26 flags" {
+  It "parses 26 flags from the supplied make help output" {
     $flags = Get-ParsedHelpFlag -HelpData (Get-Content "$PSScriptRoot\TextTests\make.txt")
     $flags.Count | Should -Be 26
   }
 
-  It "The supplied dfu-util help output contains 20 flags" {
+  It "parses 20 flags from the supplied dfu-util help output" {
     $flags = Get-ParsedHelpFlag -HelpData (Get-Content "$PSScriptRoot\TextTests\dfu-util.txt")
     $flags.Count | Should -Be 20
   }
 
-  It "The supplied gcc help output contains 37 flags" {
+  It "parses 37 flags from the supplied gcc help output" {
     $flags = Get-ParsedHelpFlag -HelpData (Get-Content "$PSScriptRoot\TextTests\gcc.txt")
     $flags.Count | Should -Be 37
   }
 }
 
 Describe 'Get-ParsedHelpOption' {
-  It "The supplied cmake help output contains 50 options" {
+  It "parses 50 options from the supplied cmake help output" {
     $options = Get-ParsedHelpOption -HelpData (Get-Content "$PSScriptRoot\TextTests\cmake.txt")
     $options.Count | Should -Be 50
   }
 
-  It "The supplied make help output contains 35 options" {
+  It "parses 35 options from the supplied make help output" {
     $options = Get-ParsedHelpOption -HelpData (Get-Content "$PSScriptRoot\TextTests\make.txt")
-    $options.Count | Should -Be 35
+    $expectedOptions = @(
+      '--always-make',
+      '--assume-new=',
+      '--assume-old=',
+      '--check-symlink-times',
+      '--debug',
+      '--directory=',
+      '--dry-run',
+      '--environment-overrides',
+      '--file=',
+      '--help',
+      '--ignore-errors',
+      '--include-dir=',
+      '--jobs',
+      '--just-print',
+      '--keep-going',
+      '--load-average',
+      '--makefile=',
+      '--max-load',
+      '--new-file=',
+      '--no-builtin-rules',
+      '--no-builtin-variables',
+      '--no-keep-going',
+      '--no-print-directory',
+      '--old-file=',
+      '--print-data-base',
+      '--print-directory',
+      '--question',
+      '--quiet',
+      '--recon',
+      '--silent',
+      '--stop',
+      '--touch',
+      '--version',
+      '--warn-undefined-variables',
+      '--what-if='
+    )
+    $options.Count | Should -Be $expectedOptions.Count
+    ($options.Param -join ' ') -eq ($expectedOptions -join ' ') | Should -Be $true
   }
 
-  It "The supplied dfu-util help output contains 20 options" {
+  It "parses 20 options from the supplied dfu-util help output" {
     $options = Get-ParsedHelpOption -HelpData (Get-Content "$PSScriptRoot\TextTests\dfu-util.txt")
     $options.Count | Should -Be 20
   }
 
-  It "The supplied gcc help output contains 5 options" {
+  It "parses 5 options from the supplied gcc help output" {
     $options = Get-ParsedHelpOption -HelpData (Get-Content "$PSScriptRoot\TextTests\gcc.txt")
     $options.Count | Should -Be 5
+  }
+}
+
+Describe 'Get-ParsedHelpParamValue' {
+  It "parses 8 values from the supplied gcc help line" {
+    $commandAst = [MockCommandAst]::new("gcc --help=")
+    $expectedValues = @(
+        'common',
+        'optimizers',
+        'params',
+        'target',
+        'warnings',
+        'joined',
+        'separate',
+        'undocumented')
+    $resultPrefix = ''
+    $values = Get-ParsedHelpParamValue `
+      -HelpLine "--help={common|optimizers|params|target|warnings|[^]{joined|separate|undocumented}}[,...]" `
+      -WordToComplete $commandAst.CommandElements[-1] `
+      -CommandAst $commandAst `
+      -CursorPosition $commandAst.ToString().Length `
+      -ParamValueAssignment `
+		  -ResultPrefix ([ref]$resultPrefix)
+
+    $values.Count | Should -Be $expectedValues.Count
+    ($values -join ' ') -eq ($expectedValues -join ' ') | Should -Be $true
   }
 }
